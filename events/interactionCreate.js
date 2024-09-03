91ea949e6e938911e75148f6fa5b970a8ae82b5d
yun2397 <95332329+yun2397@users.noreply.github.com>
2024-09-04 03:46:13 +0900

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -27,7 +27,7 @@ const fs = require("fs");
 module.exports = async (client, interaction) => {
   try {
     if (!interaction?.guild) {
-      return interaction?.reply({ content: "이 명령어는 서버에서만 사용할 수 있어요.", ephemeral: true });
+      return interaction?.reply({ content: "This command can only be used in a server.", ephemeral: true });
     } else {
       function cmd_loader() {
         if (interaction?.type === InteractionType.ApplicationCommand) {
@@ -40,10 +40,10 @@ module.exports = async (client, interaction) => {
                   if (interaction?.member?.permissions?.has(props?.permissions || "0x0000000000000800")) {
                     return props.run(client, interaction);
                   } else {
-                    return interaction?.reply({ content: `이 명령어를 사용할 권한이 없어요.`, ephemeral: true });
+                    return interaction?.reply({ content: `You don't have permission to use this command.`, ephemeral: true });
                   }
                 } catch (e) {
-                  return interaction?.reply({ content: `❌ 앗, 오류..: ${e.message}`, ephemeral: true });
+                  return interaction?.reply({ content: `❌ Error: ${e.message}`, ephemeral: true });
                 }
               }
             });
