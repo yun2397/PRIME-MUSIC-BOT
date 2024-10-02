18f3e6778d1a67b8927c31f0fb00b902e9e9798c
yun2397 <95332329+yun2397@users.noreply.github.com>
2024-10-03 00:18:45 +0900

    Update config.js

--- config.js
+++ config.js
@@ -41,9 +41,9 @@ module.exports = {
   nodes: [
     {
         name: "Node #1",
-        host: "37.114.42.191",
-        port: 6767,
-        password: "danteisnttaken",
+        host: "lava-v3.ajieblogs.eu.org",
+        port: 80,
+        password: "https://dsc.gg/ajidevserver",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
