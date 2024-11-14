ed3b96f0c1109e41d8a758ab7b1e03a76ee0a124
yun2397 <95332329+yun2397@users.noreply.github.com>
2024-11-14 17:36:54 +0900

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  musicardTheme:"themes15", //Goes from themes1 to themes19
+  musicardTheme:"themes16", //Goes from themes1 to themes19
   activityName: "You", // This is bot status Write Anything here 
   activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
