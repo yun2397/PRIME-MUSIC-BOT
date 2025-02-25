603e6b5689bbe078facd834651c4853905ca7b63
yun2397 <95332329+yun2397@users.noreply.github.com>
2025-02-26 03:37:47 +0900

    Update config.js

--- config.js
+++ config.js
@@ -38,11 +38,18 @@ module.exports = {
 
   nodes: [
      {
-      name: "XX",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port:  7019,
-      secure: false
+  "identifier": "ChalresNaig Node",
+  "password": "NAIGLAVA-dash.techbyte.host",
+  "host": "lavahatry4.techbyte.host",
+  "port": 3000,
+  "secure": false
+              },
+    {
+  "name": "ChalresNaig Node",
+  "password": "NAIGLAVA-dash.techbyte.host",
+  "host": "lavahatry4.techbyte.host",
+  "port": 3000,
+  "secure": false
     }
   ]
 }
